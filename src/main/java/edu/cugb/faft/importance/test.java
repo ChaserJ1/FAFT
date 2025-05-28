567346b2e6af34aff953d71f8f40c2d8e12a83d9
java_001111 <1093493967@qq.com>
2025-05-28 22:12:52 +0800

    Initial commit

---
+++ src/main/java/edu/cugb/faft/importance/test.java
@@ -0,0 +1,22 @@
+package edu.cugb.faft.importance;
+
+import java.util.*;
+
+/**
+ * @Author: pengjia
+ * @Description:
+ */
+public class test {
+    public static void main(String[] args) {
+        Map<String, List<String>> dag = new HashMap<>();
+        dag.put("Source1", List.of("A"));
+        dag.put("A", List.of("B", "C"));
+        dag.put("B", List.of("Sink"));
+        dag.put("C", List.of("D"));
+        dag.put("D", List.of("Sink"));
+        dag.put("Sink", new ArrayList<>());
+
+        NodeImportanceEvaluator evaluator = new NodeImportanceEvaluator(dag);
+        evaluator.evaluate();
+    }
+}
